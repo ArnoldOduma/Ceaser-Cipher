523be452ade4a739a2b022465c8febf7805ffbda
Arnold Oduma <arnoldcliff98@gmail.com>
2019-06-15 23:51:04 +0300

    Created a decrypt method that decrypts the encrypted message provided the same key that was used to encrypt it is entered

--- src/main/java/models/Decrypt.java
+++ src/main/java/models/Decrypt.java
@@ -1,5 +1,17 @@
 package models;
 
 public class Decrypt {
+    public String decrypter(){
+        String decryptedText = "";
+        CeaserCipher newCeaser = new CeaserCipher();
+        newCeaser.setmWord("ij");
+        newCeaser.setmKey(1);
 
+        for (int i = 0; i < newCeaser.getmWord().length(); i++){
+            char wordChar = newCeaser.getmWord().charAt(i);
+            char decrypted = (char) (wordChar - newCeaser.getmKey());
+            decryptedText += decrypted;
+        }
+        return decryptedText;
+    }
 }
