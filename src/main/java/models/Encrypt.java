647a6ac7cb17dcb35afe183486a1b6f0be4c5a29
Arnold Oduma <arnoldcliff98@gmail.com>
2019-06-15 23:51:49 +0300

    created a test for the decrypt method to test for functionality of the decrypt function

--- src/main/java/models/Encrypt.java
+++ src/main/java/models/Encrypt.java
@@ -2,8 +2,18 @@ package models;
 
 public class Encrypt {
 
-    public void Encrypt(){
+    public String encrypter(){
+        CeaserCipher newCeaser = new CeaserCipher();
+        StringBuilder encryptedText = new StringBuilder();
+        newCeaser.setmWord("hi");
+        newCeaser.setmKey(1);
 
+        for(int i = 0; i < newCeaser.getmWord().length(); i++ ){
+            char wordChar = newCeaser.getmWord().charAt(i);
+            char encrypted = (char) (wordChar + newCeaser.getmKey());
+            encryptedText.append(encrypted);
+        }
+        return encryptedText.toString();
     }
 
 }
