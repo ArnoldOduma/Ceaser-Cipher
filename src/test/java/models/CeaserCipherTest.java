3951774910908de166d7084376a5addaae6c4266
Arnold Oduma <arnoldcliff98@gmail.com>
2019-06-15 22:51:09 +0300

    Created a test to test if the getmWord method functions correctly

--- src/test/java/models/CeaserCipherTest.java
+++ src/test/java/models/CeaserCipherTest.java
@@ -11,5 +11,9 @@ public class CeaserCipherTest {
         assertTrue(testCeaser instanceof CeaserCipher);
     }
 
-
+    @Test
+    public void ceaserCipher_getmWord_int() {
+        CeaserCipher testCeaser = new CeaserCipher("Word",2);
+        assertEquals(2,testCeaser.getmKey());
+    }
 }
