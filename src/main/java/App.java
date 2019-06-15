05570c7a1454feec3211caa942710180ec6296da
Arnold Oduma <arnoldcliff98@gmail.com>
2019-06-15 23:57:06 +0300

    Created a welcome message in the front end app.java and added a while loop where my front end section of the game will exist

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,17 @@
 public class App {
+    public static void main (String[] args){
+        System.out.println("\t===========================");
+        System.out.println("\t\tCEASER-CIPHER");
+        System.out.println("\t===========================");
+        System.out.println("\n Welcome to Ceaser Cipher Where encryption is our drive");
+
+        boolean run = true;
+        while (run){
+
+        }
+
+
+
+    }
+
 }
