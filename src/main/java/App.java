b1722c8ab63cd897821f549fa5bd59a40076fd76
Arnold Oduma <arnoldcliff98@gmail.com>
2019-06-16 00:00:55 +0300

    Created a prompt to prompt a user to choose which section of the app they would like to access and used a scanner to get their input

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class App {
     public static void main (String[] args){
         System.out.println("\t===========================");
@@ -7,6 +9,13 @@ public class App {
 
         boolean run = true;
         while (run){
+            Scanner myScanner = new Scanner(System.in);
+            System.out.println("\n\n\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            System.out.println("\t| Enter ->  1: to Encrypt\n\t| Enter ->  2: to Decrypt\n\t| Enter ->  0: to Exit ");
+            System.out.println("\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Integer branch = Integer.parseInt(myScanner.nextLine());
+
+
 
         }
 
