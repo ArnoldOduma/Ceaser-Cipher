5e0531f30b4d6d3c2a31f08906a0b1970b299e0d
Arnold Oduma <arnoldcliff98@gmail.com>
2019-06-15 23:39:31 +0300

    Created a test for the encrypter method to check if it encrypts the passed word correctly

--- src/test/java/models/CeaserCipherTest.java
+++ src/test/java/models/CeaserCipherTest.java
@@ -7,26 +7,47 @@ import static org.junit.Assert.*;
 public class CeaserCipherTest {
     @Test
     public void ceaserCipher_instantiatesCorrectly_true() {
-        CeaserCipher testCeaser = new CeaserCipher("Word",2);
+        CeaserCipher testCeaser = new CeaserCipher();
         assertTrue(testCeaser instanceof CeaserCipher);
     }
 
     @Test
-    public void ceaserCipher_getmWord_int() {
-        CeaserCipher testCeaser = new CeaserCipher("Word",2);
+    public void ceaserCipher_getmKey_int() {
+        CeaserCipher testCeaser = new CeaserCipher();
+        testCeaser.setmKey(2);
         assertEquals(2,testCeaser.getmKey());
     }
 
     @Test
-    public void ceaserCipher_getmKey_String() {
-        CeaserCipher testCeaser = new CeaserCipher("Hello",3);
+    public void ceaserCipher_getmWord_String() {
+        CeaserCipher testCeaser = new CeaserCipher();
+        testCeaser.setmWord("Hello");
         assertEquals("Hello",testCeaser.getmWord());
     }
 
     @Test
-    public void ceaserCipher_setmWoord_String() {
-        CeaserCipher testCeaser = new CeaserCipher("Hello",3);
+    public void ceaserCipher_setmWord_String() {
+        CeaserCipher testCeaser = new CeaserCipher();
         testCeaser.setmWord("hi");
         assertEquals("hi",testCeaser.getmWord());
     }
+
+    @Test
+    public void ceaserCipher_setmKey_int(){
+        CeaserCipher testCeaser = new CeaserCipher();
+        testCeaser.setmKey(1);
+        assertEquals(1,testCeaser.getmKey());
+    }
+
+    @Test
+    public void ceaseCipher_encrypter_String(){
+        Encrypt testEncrypt = new Encrypt();
+        assertEquals("ij",testEncrypt.encrypter());
+    }
+
+
+
+
+
+
 }
